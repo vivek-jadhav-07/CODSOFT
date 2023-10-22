746573ced7df4605c7166f5e890621f400600a92
Vivek Jadhav <126957039+vivek-jadhav-07@users.noreply.github.com>
2023-10-23 01:00:58 +0530

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char op;
+    double num1, num2, result;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    cout << "Enter an operator (+, -, *, /): ";
+    cin >> op;
+
+    switch(op) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 == 0) {
+                cout << "Error! Division by zero is not allowed." << endl;
+                return -1;
+            }
+            result = num1 / num2;
+            break;
+        default:
+            cout << "Error! Invalid operator." << endl;
+            return -1;
+    }
+
+    cout << "Result: " << result << endl;
+
+    return 0;
+}
