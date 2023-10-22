4ba9bd2d5502ff50648f4849080d205dfe560c41
Vivek Jadhav <126957039+vivek-jadhav-07@users.noreply.github.com>
2023-10-23 00:59:14 +0530

    num_guessing_game.cpp

--- num_guessing_game.cpp
+++ num_guessing_game.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 
+int main() {
+    srand(time(0)); // Seed for random number generation
+    int randomNumber = (rand() % 50) + 1; // Generating random number between 1 to 50
+
+    int guessedNumber;
+    int attempts = 0;
+
+    std::cout << "Welcome to the Number Guessing Game!" << std::endl;
+
+    do {
+        std::cout << "Guess a number between 1 to 50: ";
+        std::cin >> guessedNumber;
+
+        if (guessedNumber > randomNumber) {
+            std::cout << "Too high! Try again." << std::endl;
+        } else if (guessedNumber < randomNumber) {
+            std::cout << "Too low! Try again." << std::endl;
+        }
+
+        attempts++;
+    } while (guessedNumber != randomNumber);
+
+    std::cout << "Congratulations! You guessed the correct number " << randomNumber << " in " << attempts << " attempts." << std::endl;
+
+    return 0;
+}
